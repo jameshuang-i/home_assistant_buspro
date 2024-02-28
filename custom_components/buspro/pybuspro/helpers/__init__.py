646ebe88f71ae10cb1b049299f923e3e2c3057e3
jameshuang-i <jameshuang@mail.com>
2024-02-28 21:12:51 +0800

    fix a bug

--- custom_components/buspro/pybuspro/helpers/__init__.py
+++ custom_components/buspro/pybuspro/helpers/__init__.py
@@ -3,8 +3,8 @@ import typing
 def copy_class_attrs(source, target):
     source_dict = vars(source)
     target_dict = vars(target)
-    source_attrs = [k for k in source_dict.keys() if k.startswith("_")]
-    target_attrs = [k for k in target_dict.keys() if k.startswith("_")]
+    source_attrs = [k for k in source_dict.keys() if k.startswith("_") and k!='_payload']
+    target_attrs = [k for k in target_dict.keys() if k.startswith("_") and k!='_payload']
 
     for target_attr in target_attrs:
         if target_attr in source_attrs:
